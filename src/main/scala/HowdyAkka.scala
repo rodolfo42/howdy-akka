45975565dc4bfbf9e92cee3c2be4ff5773bcd82d
Rodolfo Ferreira <rodolfo.ferreira@bluesoft.com.br>
2014-06-02 08:33:48 -0300

    simple actor

--- src/main/scala/HowdyAkka.scala
+++ src/main/scala/HowdyAkka.scala
@@ -1,3 +1,19 @@
+import akka.actor.{ActorSystem, ActorLogging, Actor, Props}
+
+case object Pint
+
+class Person extends Actor with ActorLogging {
+  def receive = {
+    case Pint => log.info("Thanks for the pint")
+  }
+}
+
 object HowdyAkka extends App {
-  println("Howdy Akka")
+  val system = ActorSystem("howdy-akka")
+
+  val alice = system.actorOf(Props(new Person), "alice")
+
+  alice ! Pint
+
+  system.shutdown()
 }
