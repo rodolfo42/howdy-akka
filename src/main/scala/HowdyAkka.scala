736861c37d0cd89743a370c337364962ad6b0184
Rodolfo Ferreira <rodolfo.ferreira@bluesoft.com.br>
2014-06-02 08:35:07 -0300

    multiple actors

--- src/main/scala/HowdyAkka.scala
+++ src/main/scala/HowdyAkka.scala
@@ -1,19 +1,48 @@
 import akka.actor.{ActorSystem, ActorLogging, Actor, Props}
 
-case object Pint
+case object Ticket
+case object FullPint
+case object EmptyPint
+
+class BarTender extends Actor with ActorLogging {
+  def receive = {
+    case Ticket =>
+      log.info("1 pint coming right up")
+
+      Thread.sleep(1000)
+
+      log.info("Your pint is ready, here you go")
+
+      sender ! FullPint
+
+    case EmptyPint =>
+      log.info("I think you're done for the day")
+
+      context.system.shutdown()
+  }
+}
 
 class Person extends Actor with ActorLogging {
   def receive = {
-    case Pint => log.info("Thanks for the pint")
+    case FullPint =>
+      log.info("I'll make short work of this")
+
+      Thread.sleep(1000)
+
+      log.info("I'm ready for the next")
+
+      sender ! EmptyPint
   }
 }
 
 object HowdyAkka extends App {
   val system = ActorSystem("howdy-akka")
 
+  val zed = system.actorOf(Props(new BarTender), "zed")
+
   val alice = system.actorOf(Props(new Person), "alice")
 
-  alice ! Pint
+  zed.tell(Ticket, alice)
 
-  system.shutdown()
+  system.awaitTermination()
 }
